8374bdd7be446d5975686727d75f9388d3a06566
Onur DEGERLI <onurdegerli@gmail.com>
2021-05-10 23:37:35 +0300

    Integrating change update for reacth

--- api/src/index.js
+++ api/src/index.js
@@ -3,8 +3,23 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+const sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database(":memory:", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQlite database.");
+});
+
 app.get("/", (req, res) => {
-  res.send("Page working!");
+  res.send("Page workingddd dddxxxddddaaaaaaeeee!");
+});
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
 });
 
 app.listen(port, () => {
