3fc80d37bef19a81189eecb2570e2de7f2cdc134
Onur DEGERLI <onurdegerli@gmail.com>
2021-05-13 12:10:03 +0300

    Implementing typescript to node

---
+++ api/dist/repositories/Question.js
@@ -0,0 +1,17 @@
+const getFirstQuestion = function (db) {
+    const sql = `SELECT id, question, question_body, question_raw, answers, correct_answer 
+              FROM zend_test_question 
+              ORDER BY id ASC
+              LIMIT 1`;
+    db.get(sql, (err, row) => {
+        if (err) {
+            throw err;
+        }
+        return row;
+    });
+};
+module.exports = {
+    getFirstQuestion
+    // getNextQuestion
+};
+//# sourceMappingURL=Question.js.map
---
+++ api/src/index.ts
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const questionRepository = require("./repositories/Question");
+
+const app = express();
+const port = 8080;
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("/db/quiz.db", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the database.");
+});
+
+app.get("/api/question", (req, res) => {
+  console.log("11111");
+  const row = questionRepository.getFirstQuestion(db);
+  console.log(row);
+  res.status(200).send(row).end();
+  console.log("3333");
+});
+
+app.get("/api/question/:id", (req, res) => {
+  const row = questionRepository.getNextQuestion(db, req.params.id);
+  res.status(200).send(row).end();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
